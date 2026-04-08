e9768a798a5851c4c6921de0df9a92fbc5c5e965
sherinsuresh05-hue <sherinsuresh05@gmail.com>
2026-04-08 21:23:45 +0530

    Create project3_sql.sql
    Added Project 3 Azure SQL Database

---
+++ project3_sql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Students (
+    ID INT,
+    Name VARCHAR(50),
+    Course VARCHAR(50)
+);
+
+INSERT INTO Students VALUES (1, 'Sherin', 'Cloud Computing');
+INSERT INTO Students VALUES (2, 'Alex', 'Data Science');
